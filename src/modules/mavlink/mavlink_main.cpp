006b8583a451c0574b55bfd54f3b9d0e95f1a28f
Fran <francesc.viaplana@aldoratech.com>
2022-04-20 15:31:36 +0200

    Testing aux servo output

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1714,7 +1714,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("HOME_POSITION", 1.0f);
 		configure_stream_local("PING", 0.1f);
 		configure_stream_local("RC_CHANNELS", 5.0f);
-		configure_stream_local("SERVO_OUTPUT_RAW_0", 3.0f);
+		// configure_stream_local("SERVO_OUTPUT_RAW_0", 3.0f);
 		configure_stream_local("SERVO_OUTPUT_RAW_1", 3.0f);
 		configure_stream_local("SYS_STATUS", 5.0f);
 		configure_stream_local("VFR_HUD", 5.0f);
